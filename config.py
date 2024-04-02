96c3fe7beda6ff249386683db8b0f5b6967f0be2
rihankumar <132763286+rihankumar@users.noreply.github.com>
2024-04-02 08:58:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
 import os
 
-API_ID = API_ID = 27464924
+API_ID = API_ID = Your api id
 
-API_HASH = os.environ.get("API_HASH", "f28d089001481597f045b70a0daa73e4")
+API_HASH = os.environ.get("API_HASH", "Your api hash")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7169219459:AAGZ7ZfhJ9O4DwxGOZLhmjqKeoHqUiyimWs")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "Your bot token")
 
 PASS_DB = int(os.environ.get("PASS_DB", "100"))
 
-OWNER = int(os.environ.get("OWNER", 6226272085))
+OWNER = int(os.environ.get("OWNER", Your user id ))
 
 LOG = -1002008011161            #don't change it otherwise you face error while deploying.
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "6226272085").split()):
+    for x in (os.environ.get("ADMINS", "Your user id").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
