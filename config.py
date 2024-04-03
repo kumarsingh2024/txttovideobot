679a95e9e2e84ec4badb274f6c9c313f25d0f64f
kumar azad <155668421+kumarsingh2024@users.noreply.github.com>
2024-04-03 18:59:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,9 +8,9 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "7014636983:AAELoi5MU1nTuS91Uh_RgZG2vBk_
 
 PASS_DB = int(os.environ.get("PASS_DB", "100"))
 
-OWNER = int(os.environ.get("OWNER", 5630726804 ))
+OWNER = int(os.environ.get("OWNER", 5630726804))
 
-LOG = -1002008011161            #don't change it otherwise you face error while deploying.
+LOG = -1001934236256            #don't change it otherwise you face error while deploying.
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "5630726804").split()):
